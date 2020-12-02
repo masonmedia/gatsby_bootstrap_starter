3188730aaa11076cc63732fa7d94069b08c05f8a
Andrew Mason <andrewmasonmedia@gmail.com>
2020-12-02 13:17:24 -0500

    Commented index.js, removed images

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,17 @@ export default function Home({data}) {
     <>
       <Layout>
         <Row className="min-h-100 p-0 m-0">
-          <Img 
+          {/* sample gatsby-image component */}
+          {/* <Img 
           className="img-full" 
           durationFadeIn={1500}
           placeholderStyle={{ backgroundColor: `black` }}
           style={{position: 'absolute', right: '0', zIndex: '0'}}
-          fluid={data.imageOne.childImageSharp.fluid} alt="" />
+          fluid={data.imageOne.childImageSharp.fluid} 
+          alt="" /> */}
           <Col sm={12} lg={7}
           className="flex-left p-5">
-            <h1 className="site-title text-lowercase">mason</h1>
+            <h1 className="site-title text-lowercase">Gatsby Bootstrap Starter</h1>
           </Col>
         </Row>
       </Layout>
@@ -25,10 +27,10 @@ export default function Home({data}) {
   ) 
 }
 
-
+// sample image query
 export const pageQuery = graphql`
   query {
-    imageOne: file(relativePath: { eq: "mm_banner.png" }) {
+    imageOne: file(relativePath: { eq: "your-image-path.png" }) {
       childImageSharp {
         fluid(maxWidth: 2000, quality: 100) {
           ...GatsbyImageSharpFluid
